161d2e32dfa994b7c78b9210c9bfd655ac342990
Emil Enriquez <emil.nrqz@gmail.com>
2024-02-26 19:27:20 +0800

    chore: initialize project with base structure and hapi framework

---
+++ server/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Glue = require('@hapi/glue');
+const Exiting = require('exiting');
+const Manifest = require('./manifest');
+const { convertObjectKeys, toCamelCase } = require('../lib/utils');
+
+exports.deployment = async ({ start } = {}) => {
+
+  const manifest = Manifest.get('/', process.env);
+  const server = await Glue.compose(manifest, { relativeTo: __dirname });
+
+  server.ext('onPreHandler', (request, h) => {
+    if (request.payload) {
+      request.payload = convertObjectKeys(request.payload, toCamelCase);
+    }
+
+    return h.continue;
+  });
+
+  if (start) {
+    await Exiting.createManager(server).start();
+    server.log(['start'], `Server started at ${server.info.uri}`);
+    return server;
+  }
+
+  await server.initialize();
+
+  return server;
+};
+
+if (require.main === module) {
+
+  exports.deployment({ start: true });
+
+  process.on('unhandledRejection', err => {
+
+    throw err;
+  });
+}
---
+++ server/manifest.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const Confidence = require('@hapipal/confidence');
+const Toys = require('@hapipal/toys');
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+  server: {
+    host: 'localhost',
+    port: {
+      $param: 'PORT',
+      $coerce: 'number',
+      $default: 3000
+    },
+    debug: {
+      $filter: 'NODE_ENV',
+      $default: {
+        log: ['error', 'start'],
+        request: ['error']
+      },
+      production: {
+        request: ['implementation']
+      }
+    },
+  },
+  register: {
+    plugins: [
+      {
+        plugin: '../lib', // Main plugin
+        options: {}
+      },
+      {
+        plugin: './plugins/swagger'
+      },
+      {
+        plugin: '@hapipal/schwifty',
+        options: {
+          $filter: 'NODE_ENV',
+          $default: {},
+          $base: {
+            migrateOnStart: true,
+            knex: {
+              client: 'pg',
+              connection: {
+                host: process.env.DB_HOST || '',
+                user: process.env.DB_USERNAME || '',
+                password: process.env.DB_PASSWORD || '',
+                database: process.env.DB_NAME || ''
+              },
+              seeds: {
+                directory: './lib/seeds'
+              }
+            }
+          },
+          development: {
+            knex: {
+              client: 'pg',
+              connection: {
+                host: process.env.DB_HOST || 'localhost',
+                user: process.env.DB_USERNAME || 'postgreslocal',
+                password: process.env.DB_PASSWORD || 'postgreslocal',
+                database: process.env.DB_NAME || 'gforms'
+              },
+              seeds: {
+                directory: './lib/seeds'
+              },
+              debug: true
+            }
+          },
+          test: {
+            migrateOnStart: true,
+            knex: {
+              client: 'pg',
+              connection: {
+                host: process.env.DB_HOST || 'localhost',
+                user: process.env.DB_USERNAME || 'postgreslocal',
+                password: process.env.DB_PASSWORD || 'postgreslocal',
+                database: process.env.DB_NAME || 'gforms'
+              },
+              seeds: {
+                directory: './lib/seeds'
+              },
+              debug: true
+            }
+          },
+          production: {
+            migrateOnStart: false,
+            knex: {
+              client: 'pg',
+              connection: {
+                host: process.env.DB_HOST || '',
+                user: process.env.DB_USERNAME || '',
+                password: process.env.DB_PASSWORD || '',
+                database: process.env.DB_NAME || ''
+              },
+              seeds: {
+                directory: './lib/seeds'
+              },
+              debug: true
+            }
+          }
+        }
+      },
+      {
+        plugin: {
+          $filter: 'NODE_ENV',
+          $default: '@hapipal/hpal-debug',
+          production: Toys.noop
+        }
+      }
+    ]
+  }
+});
