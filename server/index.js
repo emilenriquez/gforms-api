c772dd17c8f913845ed0d4f5789b8374bb602ae2
Emil Enriquez <emil.nrqz@gmail.com>
2024-02-29 15:44:24 +0800

    chore: create an interceptor where the provided payload and params are converted to camelcase automatically

--- server/index.js
+++ server/index.js
@@ -4,11 +4,23 @@ const Glue = require('@hapi/glue');
 const Exiting = require('exiting');
 const Manifest = require('./manifest');
 
+const { convertObjectKeys, toCamelCase } = require('../lib/utils');
+
 
 exports.deployment = async ({ start } = {}) => {
 
   const manifest = Manifest.get('/', process.env);
   const server = await Glue.compose(manifest, { relativeTo: __dirname });
+
+  server.ext('onPreHandler', (request, h) => {
+    if (request.payload) {
+      request.payload = convertObjectKeys(request.payload, toCamelCase);
+    }
+
+    return h.continue;
+  });
+
+
 
 
 
